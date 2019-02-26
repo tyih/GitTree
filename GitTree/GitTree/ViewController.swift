f9a6e810c48c22e97fe09dbf7c60c7bcfc810452
tyih <tyih@icloud.com>
2019-02-26 17:57:13 +0800

    init project

---
+++ GitTree/GitTree/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTree
+//
+//  Created by tianyao on 2019/2/26.
+//  Copyright © 2019 tianyao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
